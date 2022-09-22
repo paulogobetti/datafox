fcad937279900969d44b67c71c706a14f08b2094
Paulo Gobetti <paulogobetti@outlook.com.br>
2022-09-22 09:18:35 -0300

    Basic table structure

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,26 @@
         { id: 20, name: 'Kuririn', children: [], parent: null }
     ]
 
-    const addPerson = (e) => {
+    const listTable = () => {
+        // alert('LISTAR TABELA')
+    }
+
+    const addParent = (e) => {
         e.preventDefault()
 
-        let person = {
+        let parent = {
             id: Date.now(),
             name: document.getElementById('name').value,
             children: [],
             parent: null
         }
 
-        if(person.name == '') {
+        if(parent.name == '') {
             alert('Oops! Something needs to be filled in here.')
         } else {
-            people.push(person)
+            people.push(parent)
+
+            listTable()
         }
 
         document.forms[0].reset()
@@ -30,12 +36,39 @@
         document.getElementById('text-area').value = JSON.stringify(people, '\t', 2)
     }
 
+    const addChild = () => {
+        let filho = prompt('Insert a child:')
+        alert('The object child name is: ' + filho)
+    }
+
     // people.forEach(i => {
     //     console.log('Hello')
     // })
 
+    const removeParent = () => {
+        alert('removeParent')
+    }
+
+    const removeChild = () => {
+        alert('removeChild')
+    }
+
+    listTable()
+
     document.getElementById('text-area').value = JSON.stringify(people, '\t', 2)
 
     document.addEventListener('DOMContentLoaded', () => {
-        document.getElementById('add-person').addEventListener('click', addPerson)
+        document.getElementById('add-parent').addEventListener('click', addParent)
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementById('add-child').addEventListener('click', addChild)
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementById('remove-parent').addEventListener('click', removeParent)
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementById('remove-child').addEventListener('click', removeChild)
     })
